2374e6e1d94f605ba264a5528fb56cdaa958ea71
wigumen <martin.wigum@gmail.com>
2021-10-28 15:11:21 +0200

    ny css

--- burgir.js
+++ burgir.js
@@ -1,15 +1,26 @@
-var bodEl = document.querySelector("body");
-var button = document.querySelector("#burger");
-var menyEl = document.querySelector(".meny");
-var coverEl = document.querySelector(".cover");
+const bodEl = document.querySelector("body");
+const button = document.querySelector("#burger");
+const menyEl = document.querySelector(".meny");
+const coverEl = document.querySelector(".cover");
+const textEl = document.querySelector(".meny-text");
+
+const isMobile = false;
+
 button.addEventListener("click", meny);
 coverEl.addEventListener("click",hide);
 
 function meny(){
-    menyEl.style.width="20vw";
+    if(window.innerWidth > window.innerHeight){
+        menyEl.style.width="20vw";
+    } else {
+        menyEl.style.width="100%";
+    }
+    
     coverEl.style.display="block";
+    textEl.style.display="block";
 }
 function hide(){
     menyEl.style.width="0vw";
     coverEl.style.display="none";
+    textEl.style.display="none";
 }
